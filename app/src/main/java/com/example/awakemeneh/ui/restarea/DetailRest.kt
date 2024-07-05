6249891f777cb5804fbb64725c702aa892ad9424
tarawithoutbudimann <tamarasashikirana@mail.ugm.ac.id>
2024-07-06 01:09:28 +0700

    bind back

---
+++ app/src/main/java/com/example/awakemeneh/ui/restarea/DetailRest.kt
@@ -0,0 +1,12 @@
+package com.example.awakemeneh.ui.restarea
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.awakemeneh.R
+
+class DetailRest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_rest)
+    }
+}
--- app/src/main/java/com/example/awakemeneh/ui/restarea/RestAreaSearch.kt
+++ app/src/main/java/com/example/awakemeneh/ui/restarea/RestAreaSearch.kt
@@ -1,17 +1,20 @@
 package com.example.awakemeneh.ui.restarea
 
-import com.example.awakemeneh.ui.adapter.RestAdapter
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.awakemeneh.R
 import com.example.awakemeneh.databinding.ActivityRestAreaSearchBinding
+import com.example.awakemeneh.ui.adapter.RestAdapter
 import com.example.awakemeneh.ui.data.RestArea
 
 class RestAreaSearch : AppCompatActivity() {
 
     private lateinit var binding: ActivityRestAreaSearchBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRestAreaSearchBinding.inflate(layoutInflater)
@@ -30,5 +33,12 @@ class RestAreaSearch : AppCompatActivity() {
 
         val cardAdapter = RestAdapter(cardList1)
         recyclerView.adapter = cardAdapter
+
+        // Set click listener for back icon
+        val backIcon = binding.root.findViewById<ImageView>(R.id.backIcon)
+        backIcon.setOnClickListener {
+            // Finish the current activity and go back to the previous one
+            finish()
+        }
     }
 }
